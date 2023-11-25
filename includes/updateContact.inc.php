f50e3e669a53d0bef47e55ed3987f3fe9fef7985
IBNESSAYEH <abdellatifibnessayeh@gmail.com>
2023-11-26 00:17:49 +0100

    fonction ajouter contacts

--- includes/updateContact.inc.php
+++ includes/updateContact.inc.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     // Check if the query is executed successfully
     if (mysqli_query($conn, $sql)) {
-        header("Location: ../pages/contacts.php");
+        header("Location: ../pages/contacts.php" );
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
